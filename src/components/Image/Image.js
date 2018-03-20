cc79503696989b91c8004a17fded7efec7b87703
eyalrozenberg <37139238+eyalrozenberg@users.noreply.github.com>
2018-03-20 21:41:23 +0200

    Add files via upload

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import './Image.scss';
 
+ 
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
@@ -12,42 +13,65 @@ class Image extends React.Component {
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
+    this.rotate = this.rotate.bind(this);
     this.state = {
+                  rotation: 0,
       size: 200
-    };
+    }
   }
 
+ 
   calcImageSize() {
-    const {galleryWidth} = this.props;
+    //const {galleryWidth} = this.props;
+	const galleryWidth = document.body.clientWidth;
     const targetSize = 200;
-    const imagesPerRow = Math.round(galleryWidth / targetSize);
+    const imagesPerRow = Math.round(galleryWidth / targetSize);              
     const size = (galleryWidth / imagesPerRow);
     this.setState({
       size
     });
   }
 
+  resize(){
+  this.calcImageSize();
+  }
+
   componentDidMount() {
     this.calcImageSize();
+	window.addEventListener("resize", this.resize.bind(this));
   }
 
   urlFromDto(dto) {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+ rotate(){
+    let newRotation = this.state.rotation + 90;
+    if(newRotation >= 360){
+      newRotation = 0;
+    }
+    this.setState({
+      rotation: newRotation
+    })
+  }
+
   render() {
+                const { showing } = this.state;
+                const { rotation } =  this.state;
     return (
       <div
         className="image-root"
         style={{
+                                  display: (showing ? 'none' : 'inline-block'),
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
+          width: this.state.size,
+          height: this.state.size + 'px',
+                                  transform: `rotate(${rotation}deg)`
         }}
         >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
+        <div style={{transform: `rotate(${rotation*3}deg)`}}>
+          <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.rotate}/>
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" onClick={() => this.setState({ showing: !showing })}/>
           <FontAwesome className="image-icon" name="expand" title="expand"/>
         </div>
       </div>
@@ -55,4 +79,6 @@ class Image extends React.Component {
   }
 }
 
-export default Image;
+
+
+ export default Image;
